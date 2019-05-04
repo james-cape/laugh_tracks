b914857f881a23d76eef1c5dcdf1ba4ca3d81eee
James Cape <james.cape@gmail.com>
2019-05-04 09:11:43 -0600

    Adds new route - get /comedians/new, comedians#new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/comedians', to: 'comedians#index'
+  get '/comedians/new', to: 'comedians#new'
 
   # get '/comedians', to: 'comedians#index'
   # User Story 1
