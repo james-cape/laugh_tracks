66c5a16242009f54480052fec95305c3433236e3
James Cape <james.cape@gmail.com>
2019-05-04 09:11:14 -0600

    Adjusts new comedian test to first focus on initial page setup

--- spec/features/new_comedian_spec.rb
+++ spec/features/new_comedian_spec.rb
@@ -4,20 +4,24 @@ describe "user creates a new comedian" do
   describe "they link from the comedians index" do
     describe "they fill in a name, age, and city" do
       it "creates a new comedian" do
-        visit comedians_path
-        click_link "Create a New Comedian"
+        visit '/comedians/new'
 
-        expect(current_path).to eq(new_comedian_path)
-
-        fill_in "comedian[name]", with: "New Name!"
-        fill_in "comedian[age]", with: "New Age!"
-        fill_in "comedian[city]", with: "New City!"
-        click_on "Create Comedian"
-
-        expect(page).to have_content("New Name!")
-        expect(page).to have_content("New Age!")
-        expect(page).to have_content("New City!")
+        expect(page).to have_content("Name")
+        expect(page).to have_content("Age")
+        expect(page).to have_content("Birthplace")
       end
+
+      #   expect(current_path).to eq(new_comedian_path)
+      #
+      #   fill_in "comedian[name]", with: "New Name!"
+      #   fill_in "comedian[age]", with: "New Age!"
+      #   fill_in "comedian[city]", with: "New City!"
+      #   click_on "Create Comedian"
+      #
+      #   expect(page).to have_content("New Name!")
+      #   expect(page).to have_content("New Age!")
+      #   expect(page).to have_content("New City!")
+      # end
     end
   end
 end
