9d83b3f9747a9c3ec25dda2a3b333428c7ddd7be
James Cape <james.cape@gmail.com>
2019-05-03 20:57:14 -0600

    Write new_comedian feature test

---
+++ spec/features/new_comedian_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "user creates a new comedian" do
+  describe "they link from the comedians index" do
+    describe "they fill in a name, age, and city" do
+      it "creates a new comedian" do
+        visit comedians_path
+        click_link "Create a New Comedian"
+
+        expect(current_path).to eq(new_comedian_path)
+
+        fill_in "comedian[name]", with: "New Name!"
+        fill_in "comedian[age]", with: "New Age!"
+        fill_in "comedian[city]", with: "New City!"
+        click_on "Create Comedian"
+
+        expect(page).to have_content("New Name!")
+        expect(page).to have_content("New Age!")
+        expect(page).to have_content("New City!")
+      end
+    end
+  end
+end
