be70a444edd103e6b7c4a73acbdb9d86ba02f9ec
James Cape <james.cape@gmail.com>
2019-05-04 09:52:08 -0600

    Adds create method in controller. Create method makes use of private method

--- app/controllers/comedians_controller.rb
+++ app/controllers/comedians_controller.rb
@@ -11,4 +11,24 @@ class ComediansController < ApplicationController
     @comedian = Comedian.new
   end
 
+  def create
+    @comedian = Comedian.new(comedian_params)
+    @comedian.save
+    redirect_to comedians_path(@comedian)
+
+    # redirect_to comedian_path(@comedian)
+    # @comedian = Comedian.new
+    # @comedian.name = params[:comedian][:name]
+    # @comedian.age = params[:comedian][:age]
+    # @comedian.birthplace = params[:comedian][:birthplace]
+    # @comedian.save
+    # redirect_to comedian_path(@comedian)
+  end
+
+  private
+
+    def comedian_params
+      params.require(:comedian).permit(:name, :age, :birthplace)
+    end
+
 end
