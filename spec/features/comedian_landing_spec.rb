ac8707a7747d22cca49495d0c24d704045e7c65f
James Cape <james.cape@gmail.com>
2019-05-04 16:22:23 -0600

    Removes comment that has been resolved

--- spec/features/comedian_landing_spec.rb
+++ spec/features/comedian_landing_spec.rb
@@ -99,7 +99,6 @@ describe "user sees all comedians" do
       within "#comedian-#{comedian_2.id}" do
         expect(page).to have_content("3")
       end
-      # Why does save_and_open_page not have css styling?
     end
 
     it "displays statistics at top of page" do
