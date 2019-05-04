719b916ad91c993f2db4d47a0b2259e00b0d888e
James Cape <james.cape@gmail.com>
2019-05-04 09:51:35 -0600

    Adds post route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/comedians', to: 'comedians#index'
   get '/comedians/new', to: 'comedians#new'
+  post '/comedians', to: 'comedians#create'
 
   # get '/comedians', to: 'comedians#index'
   # User Story 1
