6811d592230f7684000ef3ce9db22f0f3e388a52
James Cape <james.cape@gmail.com>
2019-05-05 23:17:07 -0600

    Refactored routes to be just "resources :comedians"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/comedians', to: 'comedians#index'
-  get '/comedians/new', to: 'comedians#new'
-  post '/comedians', to: 'comedians#create'
+  resources :comedians
+  # get '/comedians', to: 'comedians#index'
+  # get '/comedians/new', to: 'comedians#new'
+  # post '/comedians', to: 'comedians#create'
+  # get 'comedian/:id', to: 'comedian#show', as: :show
 
 end
