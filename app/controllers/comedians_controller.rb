a4d57d343e98c3af476d7f264ac857da5bc9f996
James Cape <james.cape@gmail.com>
2019-05-05 23:19:17 -0600

    Adds show method to find just one comedian

--- app/controllers/comedians_controller.rb
+++ app/controllers/comedians_controller.rb
@@ -24,6 +24,21 @@ class ComediansController < ApplicationController
     redirect_to comedians_path
   end
 
+  def show
+    # if params[:age] == nil
+    #   @comedians     = Comedian.all
+    #   @unique_cities = @comedians.map { |comedian| comedian.birthplace}.uniq
+    #   @unique_ages   = @comedians.map { |comedian| comedian.age}.uniq.sort
+    #   @average_age   = @comedians.sum { |comedian| comedian.age} / @comedians.length
+    # else
+    #   @comedians     = Comedian.filter_by_age(params[:age])
+    #   @unique_cities = @comedians.map { |comedian| comedian.birthplace}.uniq
+    #   @unique_ages   = @comedians.map { |comedian| comedian.age}.uniq.sort
+    #   @average_age   = @comedians.sum { |comedian| comedian.age} / @comedians.length
+    # end
+    @comedian = Comedian.find(params[:id])
+  end
+
   private
 
   def comedian_params
